61065368085d8c32850909520482e556a6499596
daun Na <88607631+dawon0304@users.noreply.github.com>
2022-01-18 21:54:17 +0900

    페이지 상단 이동 버튼 만들기
    gsap & ScrollToPlugin.js를 설치하여 사용해준다.

--- js/main.js
+++ js/main.js
@@ -21,10 +21,15 @@ searchInputEl.addEventListener('blur', function() {
 });
 
 
-// badge scrolㅣ
+/**
+ * 페이지 스크롤에 따른 요소 제어
+ */
 const badgeEl = document.querySelector('header .badges');
-
+const toTopEl = document.querySelector('#to-top');
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
 window.addEventListener('scroll', _.throttle(function () {
+  // 페이지 스크롤 위치가 500px이 넘으면.
   console.log(window.scrollY);
   if (window.scrollY > 500) {
     // 배지 숨기기
@@ -33,16 +38,38 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 0,
       display:'none'
     });
+    // 상단으로 스크롤 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    });
+
+    // 페이지 스크롤 위치가 500px이 넘지 않으면
   } else {
     // 배지 보이기
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
     });
+    // 상단으로 스크롤 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
   }
 }, 300));
 // _.throttle(함수, 시간)
+// 상단으로 스크롤 버튼을 클릭하면,
 
+toTopEl.addEventListener('click', function () {
+    // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
+
+
+/**
+ * 순서대로 나타나는 기능
+ */
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
   // gsap.to(요소, 지속시간, 옵션);
@@ -150,3 +177,5 @@ spyEls.forEach(function (spyEl) {
 // 올해를 자동으로 구해주는
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear();
+
+
